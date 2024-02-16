baa13b9f580f965d84587faa04a1cdd36b07b9e2
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2024-02-17 02:16:39 +0600

    feat: added tailwindCSS and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="grid min-h-screen place-items-center bg-gray-900 text-white ">
+      <h1 className="text-center text-4xl font-bold">
+        React TypeScript TailwindCSS Templates with Vite
+      </h1>
+      <p className="text-center text-2xl">
+        Md Kawsar Islam Yeasin (@yeasin2002)
       </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
