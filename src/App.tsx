b7d8d5f93eb551cbb3ea36d15fd5bd3037d9e05d
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2024-02-28 15:01:25 +0600

    fix: react router's structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-export const App = () => {
-  return (
-    <div className="grid min-h-screen place-items-center bg-gray-900 text-white ">
-      <h1 className="text-center text-4xl font-bold">
-        React TypeScript TailwindCSS Templates with Vite
-      </h1>
-      <p className="text-center text-2xl">
-        Md Kawsar Islam Yeasin (@yeasin2002)
-      </p>
-    </div>
-  );
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home, NotFound, RootErrorBoundary } from './page';
+
+const router = createBrowserRouter([
+  { path: '/', Component: Home, errorElement: <RootErrorBoundary /> },
+  { path: '*', Component: NotFound },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 };
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
+import App from '@/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import { App } from '@/App';
 import './index.css';
-import { NotFound, RootErrorBoundary } from './page';
-
-const router = createBrowserRouter([
-  { path: '/', Component: App, errorElement: <RootErrorBoundary /> },
-  { path: '*', Component: NotFound },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />;
+    <App />
   </React.StrictMode>,
 );
