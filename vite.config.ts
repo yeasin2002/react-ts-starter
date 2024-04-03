8640db0884e229dd7cb0268fc5c265f7cf8b017b
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2024-04-03 23:59:55 +0600

    Configured vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
