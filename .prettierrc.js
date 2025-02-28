328f7a4308514138516f5714b33fe26b4e61bc3c
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-02-28 12:14:27 +0600

    chore: updated package and prettier configs

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports'],
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-};
