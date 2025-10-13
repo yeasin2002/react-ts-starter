8756af35ec4a8c74efaf9773b02bc94364f4c898
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-13 17:57:43 +0600

    chore: updated vite and eslint config

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,6 @@
-interface Props extends React.ComponentProps<'div'> {}
+import { Button } from "@/components";
+
+interface Props extends React.ComponentProps<"div"> {}
 
 export const Home = ({ ...rest }: Props) => {
   const [count, setCount] = useState(0); // no need to import react and react-router, auto-import will handle it
@@ -7,23 +9,16 @@ export const Home = ({ ...rest }: Props) => {
     setCount(count + 1);
   };
   return (
-    <div
-      className="grid min-h-screen place-items-center bg-gray-900 text-white "
-      {...rest}
-    >
+    <div className="grid min-h-screen place-items-center bg-gray-900 text-white" {...rest}>
       <div>
         <span className="mx-3 text-2xl">Count: {count}</span>
-        <Button variant={'secondary'} onClick={handleIncrement}>
+        <Button variant={"secondary"} onClick={handleIncrement}>
           Increase
         </Button>
       </div>
       <p className="text-center text-xl">
         React-TS Starter with Vite By Md Kawsar Islam Yeasin
-        <a
-          href="https://github.com/yeasin2002"
-          className="underline mx-2"
-          target="_blank"
-        >
+        <a href="https://github.com/yeasin2002" className="mx-2 underline" target="_blank">
           (yeasin2002)
         </a>
       </p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import path from 'path';
-import AutoImport from 'unplugin-auto-import/vite';
-import { defineConfig } from 'vite';
+import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import { defineConfig } from "vite";
 
 // vite plugins
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-import Fonts from 'unplugin-fonts/vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import Fonts from "unplugin-fonts/vite";
 // @ts-ignore
-import imagemin from 'unplugin-imagemin/vite';
-import { compression } from 'vite-plugin-compression2';
-import Inspect from 'vite-plugin-inspect';
-import svgr from 'vite-plugin-svgr';
+import imagemin from "unplugin-imagemin/vite";
+import { compression } from "vite-plugin-compression2";
+import Inspect from "vite-plugin-inspect";
+import svgr from "vite-plugin-svgr";
 
-import { fonts } from './configs/fonts.config';
+import { fonts } from "./configs/fonts.config";
 
 export default defineConfig({
   plugins: [
@@ -24,15 +24,21 @@ export default defineConfig({
     tailwindcss(),
     Fonts({ google: { families: fonts } }),
     AutoImport({
-      imports: ['react', 'react-router'],
-      dts: './auto-imports.d.ts',
-      eslintrc: { filepath: './eslint.config.js' },
-      dirs: ['./src/components/ui'],
+      imports: ["react", "react-router"],
+      dts: "./auto-imports.d.ts",
+      eslintrc: {
+        enabled: true,
+        // filepath: "./eslint.config.js",
+      },
+      viteOptimizeDeps: true,
+
+      // uncomment if you want to auto import ui components
+      // dirs: ['./src/components/ui'],
     }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
