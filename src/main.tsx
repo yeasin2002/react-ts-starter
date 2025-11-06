e7a5dc72d59a903f1879e01b7b1f456ec187c664
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-06 11:10:55 +0600

    chore: remove unused package

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import "./index.css";
 
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, useRoutes } from "react-router-dom";
+import { BrowserRouter, useRoutes } from "react-router";
 import routes from "~react-pages";
 
 // eslint-disable-next-line react-refresh/only-export-components
 function App() {
-  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+  return <Suspense fallback={<p>...</p>}>{useRoutes(routes)}</Suspense>;
 }
 
 const app = createRoot(document.getElementById("root")!);
